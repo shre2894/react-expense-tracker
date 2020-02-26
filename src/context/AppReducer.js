d0055051ac95b12ab1559aa78f05c356a68e46c1
shreyansh jain <sj@plustvinc.com>
2020-02-26 10:49:12 +0530

    client code

---
+++ src/context/AppReducer.js
@@ -0,0 +1,18 @@
+export default (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          transaction => transaction.id !== action.payload
+        )
+      };
+    case "ADD_TRANACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/context/GlobalState.js
@@ -0,0 +1,47 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// Intial State
+const initialState = {
+  transactions: [
+    // { id: 1, text: "Flower", amount: -20 },
+    // { id: 2, text: "Salary", amount: 300 },
+    // { id: 3, text: "Book", amount: -10 },
+    // { id: 4, text: "Camera", amount: 150 }
+  ]
+};
+
+// Create Context
+export const GlobalContext = createContext(initialState);
+
+// provider
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANACTION",
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANACTION",
+      payload: transaction
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
